f47afb54ffdf02e0d44295f0f3668fc977de4da5
Thomas Anderson <tba9h@virginia.edu>
2026-03-12 16:39:54 -0400

    Fixed some names

--- RecCalorimeter/src/components/DualCrysSiPMAlgo.h
+++ RecCalorimeter/src/components/DualCrysSiPMAlgo.h
@@ -117,7 +117,8 @@ const std::map<int, double > o58_filterMap = {{ 200, 0.0 },{ 210, 0.0 },{ 220, 0
 			       "killedScintPhotons",
 			       "Output containing the collection of killed Scintillation photons"};
   Gaudi::Property<std::string> m_killedCherenCollection{this, 
-				"killedCherenPhotonCollection", "killedCherenPhotons",
+				"killedCherenPhotonCollection",
+				"killedCherenPhotons",
 				"Output containing the collection of killed Cherenkov photons"};
 
   Gaudi::Property<std::string> m_passedScintCollection{this,
@@ -126,7 +127,7 @@ const std::map<int, double > o58_filterMap = {{ 200, 0.0 },{ 210, 0.0 },{ 220, 0
 			       "Output containing the collection of passed Scintillation photons"};
 
   Gaudi::Property<std::string> m_passedCherenCollection{this, 
-			       "killedCherenPhotonCollection",
+			       "passedCherenPhotonCollection",
 			       "passedCherenPhotons",
 			       "Output containing the collection of passed Cherenkov photons"};
 
